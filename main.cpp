e978008647358356c8de8d934098d1df0dd636de
visionsmika-gif <visionsmika@gmail.com>
2025-09-18 19:46:45 -0700

    update

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include <iostream>
 #include <array>
 #include <fstream>
+#include <iomanip>
+#include <algorithm> // for sort() and find()
+#include <numeric> // for accumulate()
 
 using namespace std;
 
@@ -13,6 +16,7 @@ int main() {
 	array<double, SIZE> prices;
 	const string FILE_NAME = "prices.txt";
 	ifstream inputFile(FILE_NAME);
+	const int CENT_DIGITS = 2; // Used for setprecision() to display cents
 
 	// Ensure that the file is open
 	if (!inputFile) {
@@ -28,11 +32,13 @@ int main() {
 	}
 	inputFile.close();
 
+	cout << fixed << setprecision(CENT_DIGITS);
+
 	// Use size() member function
 	cout << "The inventory contains a list of " << prices.size() << " prices.\n";
 
 	// Use empty() member function
-	cout << "The price list is " << (prices.empty() ? "empty" : "not empty") << ".\n";
+	cout << "The list of prices is " << (prices.empty() ? "empty" : "not empty") << ".\n";
 
 	// Use front() and back() member functions
 	cout << "The price of the first item is $" << prices.front() << ".\n";
@@ -41,6 +47,22 @@ int main() {
 	// Find the maximum, minimum, and sum of the array
 	cout << "The most expensive item costs $" << *max_element(prices.begin(), prices.end()) << ".\n";
 	cout << "The cheapest item costs $" << *min_element(prices.begin(), prices.end()) << ".\n";
+	cout << "The sum of the array is $" << accumulate(prices.begin(), prices.end(), 0.0) << ".\n";
+
+	// Use iterators to sort
+	cout << "Prices sorted from lowest to highest:\n";
+	sort(prices.begin(), prices.end());
+	for (double val : prices) {
+		cout << "$" << val << " ";
+	}
+	cout << "\n";
+
+	// Use reverse iterators to sort in reverse
+	sort(prices.rbegin(), prices.rend());
+	for (double val : prices) {
+		cout << "$" << val << " ";
+	}
+	cout << "\n";
 
 	return 0;
 }
