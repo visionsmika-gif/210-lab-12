bb158415f8c88a8929c6d4adba4de954d7ba3eca
visionsmika-gif <visionsmika@gmail.com>
2025-09-18 19:37:04 -0700

    update

--- main.cpp
+++ main.cpp
@@ -14,19 +14,33 @@ int main() {
 	const string FILE_NAME = "prices.txt";
 	ifstream inputFile(FILE_NAME);
 
+	// Ensure that the file is open
 	if (!inputFile) {
 		cout << "ERROR: Unable to open " << FILE_NAME << ". Please ensure that the file is in the same directory as the program.\n";
 		return 1;
 	}
-
 	// Read the file into the array
 	for (int i = 0; i < SIZE; ++i) {
 		if (!(inputFile >> prices[i])) {
 			cout << "ERROR: There was an error reading " << FILE_NAME << ". Please check that the file is in the proper format.\n";
+			return 1;
 		}
 	}
-
 	inputFile.close();
+
+	// Use size() member function
+	cout << "The inventory contains a list of " << prices.size() << " prices.\n";
+
+	// Use empty() member function
+	cout << "The price list is " << (prices.empty() ? "empty" : "not empty") << ".\n";
+
+	// Use front() and back() member functions
+	cout << "The price of the first item is $" << prices.front() << ".\n";
+	cout << "The price of the last item is $" << prices.back() << ".\n";
+
+	// Find the maximum, minimum, and sum of the array
+	cout << "The most expensive item costs $" << *max_element(prices.begin(), prices.end()) << ".\n";
+	cout << "The cheapest item costs $" << *min_element(prices.begin(), prices.end()) << ".\n";
 
 	return 0;
 }
