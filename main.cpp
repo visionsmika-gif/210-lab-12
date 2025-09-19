f3f932e19128fd095443c3650a0a463be7cb933a
visionsmika-gif <visionsmika@gmail.com>
2025-09-18 19:17:31 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,3 @@
+// Program that exercises STD::array functions
+// Real-world simulation of something
+// Requirements: use STD::array, have external data file with at least 30 elements, read the data into array, use a large variety of array member functions
